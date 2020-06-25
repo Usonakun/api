58601afd6e132d5389322ec0a38636f63040fc4d
uson <uson@usons-MacBook-Pro.local>
2020-06-25 00:26:55 -0400

    I completed POST, GET, PUT and DELETE calls

--- src/main/java/fun/Game.java
+++ src/main/java/fun/Game.java
@@ -1,10 +1,9 @@
 package fun;
-
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter @Builder
 public class Game {
-    private Integer id;
-    private String text;
+      private Integer id;
+      private String text;
 }
